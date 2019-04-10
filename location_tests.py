35755a4bddd93c4f0615ee201764e1a4aaebf916
gsytnik <39895377+gsytnik@users.noreply.github.com>
2019-04-10 11:23:42 -0700

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -6,6 +6,14 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        
+    def test_eq(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc2 = loc
+        loc3 = Location("CHINA", 33.3, 30.2)
+        self.assertEqual(loc2, loc)
+        self.assertNotEqual(loc3, loc)
+        self.assertNotEqual(loc2, loc3)
     
     # Add more tests!
 
